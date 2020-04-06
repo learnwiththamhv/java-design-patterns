f4de85f8b092f23fdbff83498c7eb9b2f9669c0f
learnwiththamhv <learnwiththamhv@gmail.com>
2020-04-06 15:55:12 +0700

    Builder

---
+++ src/test/java/xyz/jguru/solid/creational/builder/exercise/CodeBuilderTest.java
@@ -0,0 +1,33 @@
+package xyz.jguru.solid.creational.builder.exercise;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CodeBuilderTest {
+
+    private String preprocess(String text)
+    {
+        return text.replace("\r\n", "\n").trim();
+    }
+
+    @Test
+    public void emptyTest() {
+        CodeBuilder cb = new CodeBuilder("Foo");
+        assertEquals("public class Foo\n{\n}",
+                preprocess(cb.toString()));
+    }
+
+    @Test
+    public void personTest()
+    {
+        CodeBuilder cb = new CodeBuilder("Person")
+                .addField("name", "String")
+                .addField("age", "int");
+        assertEquals("public class Person\n{\n" +
+                        "  public String name;\n" +
+                        "  public int age;\n}",
+                preprocess(cb.toString()));
+    }
+
+}
