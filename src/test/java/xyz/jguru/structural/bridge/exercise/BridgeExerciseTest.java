fec1ba286424a1b728a8d8bbb548159c57580bb1
learnwiththamhv <learnwiththamhv@gmail.com>
2020-04-07 16:27:28 +0700

    Bridge

---
+++ src/test/java/xyz/jguru/structural/bridge/exercise/BridgeExerciseTest.java
@@ -0,0 +1,16 @@
+package xyz.jguru.structural.bridge.exercise;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BridgeExerciseTest {
+
+    @Test
+    public void test()
+    {
+        assertEquals("Drawing Square as lines",
+                new Square(new VectorRenderer()).toString());
+    }
+
+}
