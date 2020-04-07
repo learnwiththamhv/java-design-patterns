cf4ab0131bb0f15e264850dd2361fafaea2e18ec
learnwiththamhv <learnwiththamhv@gmail.com>
2020-04-07 21:48:41 +0700

    Composite

---
+++ src/test/java/xyz/jguru/structural/composite/exercise/MyListTest.java
@@ -0,0 +1,22 @@
+package xyz.jguru.structural.composite.exercise;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyListTest {
+
+    @Test
+    public void test()
+    {
+        SingleValue singleValue = new SingleValue(11);
+        ManyValues otherValues = new ManyValues();
+        otherValues.add(22);
+        otherValues.add(33);
+        assertEquals(66,
+                new MyList(List.of(singleValue, otherValues)).sum());
+    }
+
+}
