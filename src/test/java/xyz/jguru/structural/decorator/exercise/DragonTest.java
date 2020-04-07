69490cfb6f83c32b939036323d5780e690ade872
learnwiththamhv <learnwiththamhv@gmail.com>
2020-04-08 06:35:12 +0700

    Decorator

---
+++ src/test/java/xyz/jguru/structural/decorator/exercise/DragonTest.java
@@ -0,0 +1,22 @@
+package xyz.jguru.structural.decorator.exercise;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DragonTest {
+
+    @Test
+    public void test()
+    {
+        Dragon dragon = new Dragon();
+
+        assertEquals("flying", dragon.fly());
+        assertEquals("too young", dragon.crawl());
+
+        dragon.setAge(20);
+
+        assertEquals("too old", dragon.fly());
+        assertEquals("crawling", dragon.crawl());
+    }
+}
